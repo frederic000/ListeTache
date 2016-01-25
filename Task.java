4763130eb80d1271ad0cdda027ef29e12c76c5d2
frederic000 <frederic000@users.noreply.github.com>
2016-01-25 19:10:06 +0100

    Update Task.java

--- Task.java
+++ Task.java
@@ -1,10 +1,10 @@
+// une tache : un nom et un etat
 public class Task{
 
 public String label
 public Boolean
 }
 
-// nouvelle tache
 public Task(String label){
 this.label=label;
 this.state=false;
