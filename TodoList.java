34338feb65a59092e14bcca5e675009863851146
frederic000 <frederic000@users.noreply.github.com>
2016-01-25 19:07:28 +0100

    Create TodoList.java

---
+++ TodoList.java
@@ -0,0 +1,12 @@
+public class TodoList {
+
+	public static void main (String[] args){
+		TaskList list = new TaskList();
+	
+	// ajoute 2 taches....
+		list.addTask("Test");
+		list.addTask("Test 2 ");
+list.taskDone(0);
+		System.out.println(list);
+	}
+}
